64539084ab02fb3c4e2428c157807311b351ba3e
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2025-12-15 08:51:04 -0800

    .....

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import ArticlePage from "./pages/ArticlePage";
+import AddNews from "./pages/AuthorDashboard";
+import CategoryPage from "./pages/CategoryPage";
+import AdsSection from "./components/AdsSection";
+import TV from "./components/TV";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import AuthorDashboard from "./pages/AuthorDashboard";
+import PrivateRoute from "./components/PrivateRoute";
+export default function App(){
+  return (
+    <BrowserRouter>
+     <AuthProvider>
+     <Routes>
+  <Route path="/" element={<Landing/>} />
+  <Route path="/category/:name" element={<CategoryPage/>} />
+  <Route path="/article/:id" element={<ArticlePage/>} />
+ <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute roles={["admin"]}>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/author"
+            element={
+              <PrivateRoute roles={["author"]}>
+                <AuthorDashboard />
+              </PrivateRoute>
+            }
+          />
+  <Route path="/tv" element={<TV/>} />
+  <Route path="/ads" element={<AdsSection/>} />
+  
+</Routes>
+ </AuthProvider>
+    </BrowserRouter>
+  );
+}
