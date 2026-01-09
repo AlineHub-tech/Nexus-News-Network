46d48f16733f5b3372e0fa809d344c5e6817418b
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-09 18:59:35 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,7 @@
-// import React from "react";
-// import "../styles/footer.css";
-// const Footer = () => {
-//   return (
-//     <footer className="footer">
-//       <div className="footer-container">
-//         {/* Logo & Info */}
-//         <div className="footer-info">
-//           <h2>Nexus News Network</h2>
-//           <p>© 2025 Nexus News Network. All rights reserved.</p>
-//           <p>Email Us: nexusnewsnetwork1@gmail.com</p>
-//         </div>
-
-//         {/* Social Media */}
-//         <div className="footer-socials">
-//          <a href="https://www.facebook.com/profile.php?id=61566301534848&mibextid=ZbWKwL" target="_blank">
-//           <i className="fab fa-facebook"></i>
-//         </a>
-//         <a href="https://x.com/NEXUSNEWSNETWAK?t=PvqTyUXF3XBBmADoGwdg_w&s=09" target="_blank">
-//           <i className="fab fa-x"></i>
-//         </a>
-//         <a href="https://www.instagram.com/nnnrwanda?utm_source=ig_web_button_share_sheet&igsh=MXBoa2E1bXFnb3N2MA==" target="_blank">
-//           <i className="fab fa-instagram"></i>
-//         </a>
-//         {/* <a href="https://whatsapp.com/channel/0029VbAMorYCxoAzT2hmP53h" target="_blank">
-//           <i className="fab fa-message"></i>
-//         </a> */}
-//          <a href=" https://www.tiktok.com/@nexus.news.network?_t=ZM-8xy3htvIVbp&_r=1" target="_blank">
-//           <i className="fab fa-tiktok"></i>
-//         </a>
-//         <a href="youtube.com/@NexusNewsNetwork_11" target="_blank">
-//           <i className="fab fa-youtube"></i>
-//         </a>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
-
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import TermsOfService from './pages/TermsOfService';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 import { NewsContext } from "../context/NewsContext"; // Import the context
 import "../styles/footer.css";
 
@@ -178,6 +141,7 @@ export default Footer;
 
 
 // export default Footer;
+
 
 
 
