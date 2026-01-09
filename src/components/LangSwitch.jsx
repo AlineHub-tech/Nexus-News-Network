6470c0e5cc4972924aee4dd712636eb602ec5094
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-09 10:36:45 +0300

    Update LangSwitch.jsx

--- src/components/LangSwitch.jsx
+++ src/components/LangSwitch.jsx
@@ -1,4 +1,7 @@
 import React, { useContext } from "react";
+import ukflag from "../assets/ukflag.jpeg";
+import rwflag from "../assets/rwflag.jpeg";
 import "../styles/search.css";
 import { NewsContext } from "../context/NewsContext";
 export default function LangSwitch(){ const { language, switchLanguage } = useContext(NewsContext); return (<div><button onClick={()=>switchLanguage('en')}>EN</button><button onClick={()=>switchLanguage('rw')}>RW</button></div>); }
+
