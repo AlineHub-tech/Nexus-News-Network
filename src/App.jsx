9cdceea9c1e5b91d42b6c5657a7f7832dd782b74
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-10 14:48:31 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,36 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import ArticlePage from "./pages/ArticlePage";
+// import AddNews from "./pages/AuthorDashboard";
 import CategoryPage from "./pages/CategoryPage";
-
 import AdsSection from "./components/AdsSection";
 import TV from "./components/TV";
-
-import Login from "./pages/Login";
-import AdminDashboard from "./pages/AdminDashboard";
-import AuthorDashboard from "./pages/AuthorDashboard";
-
 import AboutUs from "./pages/AboutUs";
 import OurMission from "./pages/OurMission";
 import OurVision from "./pages/OurVision";
 import TermsOfService from "./pages/TermsOfService";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 
-import PrivateRoute from "./components/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
-
-function App() {
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import AuthorDashboard from "./pages/AuthorDashboard";
+import PrivateRoute from "./components/PrivateRoute";
+export default function App(){
   return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          {/* Public Pages */}
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<AboutUs />} />
+    <BrowserRouter>
+     <AuthProvider>
+     <Routes>
+  <Route path="/" element={<Landing/>} />
+            <Route path="/about" element={<AboutUs />} />
           <Route path="/mission" element={<OurMission />} />
           <Route path="/vision" element={<OurVision />} />
           <Route path="/terms" element={<TermsOfService />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/category/:name" element={<CategoryPage />} />
-          <Route path="/article/:id" element={<ArticlePage />} />
-          <Route path="/tv" element={<TV />} />
-          <Route path="/ads" element={<AdsSection />} />
-
-          {/* Protected Pages */}
+  <Route path="/category/:name" element={<CategoryPage/>} />
+  <Route path="/article/:id" element={<ArticlePage/>} />
+ <Route path="/login" element={<Login />} />
           <Route
             path="/admin"
             element={
@@ -56,10 +47,11 @@ function App() {
               </PrivateRoute>
             }
           />
-        </Routes>
-      </AuthProvider>
-    </Router>
+  <Route path="/tv" element={<TV/>} />
+  <Route path="/ads" element={<AdsSection/>} />
+  
+</Routes>
+ </AuthProvider>
+    </BrowserRouter>
   );
 }
-
-export default App;
