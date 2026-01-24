906956d8492248cc38c042fed1b058b33611a0c7
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-24 20:37:54 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js (cyangwa .ts)
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-})
+  // Ongeramo iki gice cyose kugira ngo ushiremo modules
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  // Iyi ni Alternative niba iriya yo hejuru yanze
+  // build: {
+  //   rollupOptions: {
+  //     external: ['axios'], 
+  //   },
+  // },
+});
