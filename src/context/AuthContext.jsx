8758e746338bbfa36cded809ce59633bf7aa8df4
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-27 15:58:51 +0300

    context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,7 +6,7 @@ export const AuthContext = createContext();
 
 // --- UMURONGO W'INGENZI WAKOSOWE HANO MURI ADIRESI ---
 // Turakeka ko VITE_API_URL muri Vercel ari: https://nexus-news-network-backend.onrender.com (Nta slash ku iherezo)
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || '//localhost:5000';
 
 // Hano API_AUTH_URL yakosowe kugira ngo ihuze na Server.js (app.use('/api/auth', authRoutes))
 const API_AUTH_URL = `${API_BASE_URL}/api/auth`;
