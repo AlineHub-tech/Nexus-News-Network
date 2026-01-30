57979e911a79dd926a527ac6c5d00465cfd51d2e
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-01-30 13:15:37 +0300

    LoadingScreen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -12,7 +12,7 @@ const LoadingScreen = () => {
         <div className="loading-bar">
           <div className="loading-progress"></div>
         </div>
-        <p className="loading-text">Loading Nexus News Network...</p>
+        <p className="loading-text">Nexus News Network</p>
       </div>
     </div>
   );
